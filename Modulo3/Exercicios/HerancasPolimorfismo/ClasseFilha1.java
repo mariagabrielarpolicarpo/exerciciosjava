a8503d3a6e5b00736278a1d2048073e4b57474db
mariagabrielarpolicarpo <104279006+mariagabrielarpolicarpo@users.noreply.github.com>
2022-09-12 20:06:57 -0300

    Add files via upload

---
+++ Modulo3/Exercicios/HerancasPolimorfismo/ClasseFilha1.java
@@ -0,0 +1,11 @@
+package HerançasPolimorfismo;
+
+class ClasseFilha1 extends ClasseMae {
+
+    @Override
+    void metodo1() {
+        System.out.println("Método 1 da Classe Filha 1");
+    }
+
+}
+
